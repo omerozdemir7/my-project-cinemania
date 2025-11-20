c699a080b288d19edc6fb6a6ec9654f996b7ecd6
Ömer ÖZDEMİR <ozd.omer17@gmail.com>
2025-11-20 18:34:37 +0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,21 @@
 import { defineConfig } from "vite";
-import { glob } from "glob";
 import injectHTML from "vite-plugin-html-inject";
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === "serve" ? "global" : "_global"]: {},
-    },
-    root: "src", // DİKKAT: Kök dizini 'src' olarak ayarladık
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        // src klasörü altındaki tüm html dosyalarını giriş noktası yap
-        input: glob.sync("./src/*.html"),
-        outDir: "../dist",
-      },
-    },
-    plugins: [
-      injectHTML(), // Bu eklenti <load> etiketlerini işler
-    ],
-  };
+export default defineConfig({
+  root: "src",
+
+  base: "/My-Project-Cinemania/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: "src/index.html",
+        catalog: "src/catalog.html",
+        library: "src/my-library.html"
+      }
+    }
+  },
+
+  plugins: [injectHTML()]
 });
